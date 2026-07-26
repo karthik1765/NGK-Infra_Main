ad562cf9a68567cc335acdee355ad8752e3f69b7
CHEKATI KARTHIK <187286643+karthik1765@users.noreply.github.com>
2026-07-27 00:31:24 +0530

    chore: ready for GitHub upload

--- src/app/pages/News.tsx
+++ src/app/pages/News.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Link } from "react-router";
-import { ArrowRight, Calendar } from "lucide-react";
+
+import { Calendar } from "lucide-react";
 
 const NEWS = [
   {
